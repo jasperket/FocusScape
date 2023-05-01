3c489374fe9e34f51a5b1e9e3d400f7fd42971c2
jasperket <jasperkeith1.vallecera@gmail.com>
2023-05-01 16:33:37 +0800

    Made pomodoro timer activity

---
+++ app/src/main/java/com/example/focusscape/PomodoroTimer.java
@@ -0,0 +1,98 @@
+package com.example.focusscape;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class PomodoroTimer extends AppCompatActivity {
+    private static final long START_TIME_IN_MILLIS = 600000;
+
+    private TextView mTextViewCountdown;
+    private Button mButtonStartPause;
+    private Button mButtonReset;
+
+    private CountDownTimer mCountDownTimer;
+
+    private boolean mTimerRunning;
+
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pomodoro_timer);
+
+        mTextViewCountdown = findViewById(R.id.textViewCountdown);
+        mButtonStartPause = findViewById(R.id.btnStartPause);
+        mButtonReset = findViewById(R.id.btnReset);
+
+        mButtonStartPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(mTimerRunning) {
+                    pauseTimer();
+                } else {
+                    startTimer();
+                }
+            }
+        });
+
+        mButtonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resetTimer();
+            }
+        });
+
+        updateCountDownText();
+    }
+
+    public void startTimer() {
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                mButtonStartPause.setText("Start");
+                mButtonStartPause.setVisibility(View.INVISIBLE);
+                mButtonReset.setVisibility(View.VISIBLE);
+            }
+        }.start();
+
+        mTimerRunning = true;
+        mButtonStartPause.setText("Pause");
+        mButtonReset.setVisibility(View.INVISIBLE);
+    }
+
+    public void pauseTimer() {
+        mCountDownTimer.cancel();
+        mTimerRunning = false;
+        mButtonStartPause.setText("Start");
+        mButtonReset.setVisibility(View.VISIBLE);
+    }
+
+    public void resetTimer() {
+        mTimeLeftInMillis = START_TIME_IN_MILLIS;
+        updateCountDownText();
+        mButtonReset.setVisibility(View.INVISIBLE);
+        mButtonStartPause.setVisibility(View.VISIBLE);
+    }
+
+    public void updateCountDownText() {
+        int minutes = (int) mTimeLeftInMillis / 1000 / 60;
+        int seconds = (int) mTimeLeftInMillis / 1000 % 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(),"%02d:%02d",minutes,seconds);
+        mTextViewCountdown.setText(timeLeftFormatted);
+    }
+}
