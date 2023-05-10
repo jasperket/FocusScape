a1dca3cb57a973d026770d2f1c178069a720f18f
terraknighty <johnjosephpableo@gmail.com>
2023-05-10 23:44:37 +0800

    Noise Player Activity and Resources

---
+++ app/src/main/java/com/example/focusscape/NoisePlayer.java
@@ -0,0 +1,14 @@
+package com.example.focusscape;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoisePlayer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_noise_player);
+    }
+}
